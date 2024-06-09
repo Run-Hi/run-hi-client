1e0ba7176c5a941510fddf1e31a75e403b1036c3
bo <jacobhboy66@gmail.com>
2024-06-10 01:09:00 +0900

    feat: 헤더 퍼블리싱

---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+import Logo from "@/components/assets/logo"
+import styled from "styled-components";
+import font from "@/components/fonts/boldFont";
+
+const Header = () => {
+    return <StyledHeader>
+        <Logo
+            width={130}
+            height={40}
+        />
+        <StyledString>마이페이지</StyledString>
+    </StyledHeader>
+
+
+}
+
+export default Header;
+
+const StyledHeader = styled.div`
+    height: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 40px 10px 30px;
+    border-bottom: 1px solid lightgray;
+`
+
+
+const StyledString = styled.span`
+    align-content: center;
+    right: 30px;
+    top: 20px;
+    ${font.BOLD_H5};
+`
