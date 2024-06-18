6ea1f0735f2aee98418fa26769ebdafb28d5a9e2
bo <jacobhboy66@gmail.com>
2024-06-19 08:26:10 +0900

    update: 백엔드 url 변경

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: "http://localhost:9090",
+    baseURL: "https://liner.sickgyun.kro.kr",
     headers: {
         "Contents-Type": "application/json",
     }
